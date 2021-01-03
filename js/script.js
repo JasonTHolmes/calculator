005c14f3d2ecb1438fe16b943ddf14104f1a0e47
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-01-02 19:45:51 -0800

    Working on getting animate.css figured out.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function()    {
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated animate__fadeIn');
+      }, {
+        offset: '50%';
+
+});
