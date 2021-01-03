ee3b779f9744782ebec98be97c332a14080fd67b
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-01-02 19:59:49 -0800

    Got the fadeIn to work by linking to the CDN and adding the class names manually to HTML doc. Need to figure out how to add them using JQuery.

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function()    {
-    $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated animate__fadeIn');
-      }, {
-        offset: '50%';
-
-});
