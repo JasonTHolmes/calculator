0e2b3cae81066aa518e0a0e11e63bb22a75443c7
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-01-25 14:06:36 -0800

    Added some style

--- js/script.js
+++ js/script.js
@@ -17,32 +17,32 @@ var numDecimal = document.getElementById('decimal');
 var numClear = document.getElementById('clear');
 var addSum = document.getElementById('add-sum');
 
-numOne.addEventListener('input', add);
-numTwo.addEventListener('input', add);
-numThree.addEventListener('input', add);
-numFour.addEventListener('input', add);
-numFive.addEventListener('input', add);
-numSix.addEventListener('input', add);
-numSeven.addEventListener('input', add);
-numEight.addEventListener('input', add);
-numNine.addEventListener('input', add);
-numZero.addEventListener('input', add);
-numDoubleZero.addEventListener('input', add);
-numPlus.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
-num.addEventListener('input', add);
+numOne.addEventListener('onClick', add);
+numTwo.addEventListener('onClick', add);
+numThree.addEventListener('onClick', add);
+numFour.addEventListener('onClick', add);
+numFive.addEventListener('onClick', add);
+numSix.addEventListener('onClick', add);
+numSeven.addEventListener('onClick', add);
+numEight.addEventListener('onClick', add);
+numNine.addEventListener('onClick', add);
+numZero.addEventListener('onClick', add);
+numDoubleZero.addEventListener('onClick', add);
+numPlus.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
 
 /*To change the equation type, just change out the symbol in line 16 addSum.innerText = one+two;*/
 
-function add() {
-    var one = parseFloat(numOne.value) || 0;
-    var two = parseFloat(numTwo.value) || 0;
-    addSum.innerText = one+two;
+function add(){
+    
+
+
 }
