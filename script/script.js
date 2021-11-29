ba13e7ea0ed3126aa10bb63c91c4caceb543fb9f
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-11-29 14:52:38 -0800

    Changed folder names

---
+++ script/script.js
@@ -0,0 +1,48 @@
+var numOne = document.getElementById('one');
+var numTwo = document.getElementById('two');
+var numThree = document.getElementById('three');
+var numFour = document.getElementById('four');
+var numFive = document.getElementById('five');
+var numSix = document.getElementById('six');
+var numSeven = document.getElementById('seven');
+var numEight = document.getElementById('eight');
+var numNine = document.getElementById('nine');
+var numZero = document.getElementById('zero');
+var numDoubleZero = document.getElementById('double-zero');
+var numPlus = document.getElementById('plus');
+var numMinus = document.getElementById('minus');
+var numMultiply = document.getElementById('multiply');
+var numDivide = document.getElementById('divide');
+var numDecimal = document.getElementById('decimal');
+var numClear = document.getElementById('clear');
+var addSum = document.getElementById('add-sum');
+
+numOne.addEventListener('onClick', add);
+numTwo.addEventListener('onClick', add);
+numThree.addEventListener('onClick', add);
+numFour.addEventListener('onClick', add);
+numFive.addEventListener('onClick', add);
+numSix.addEventListener('onClick', add);
+numSeven.addEventListener('onClick', add);
+numEight.addEventListener('onClick', add);
+numNine.addEventListener('onClick', add);
+numZero.addEventListener('onClick', add);
+numDoubleZero.addEventListener('onClick', add);
+numPlus.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+num.addEventListener('onClick', add);
+
+/*To change the equation type, just change out the symbol in line 16 addSum.innerText = one+two;*/
+
+function add(){
+    
+
+
+}
